b1ff1ed1942456410711bb950804cf37b5afe0e5
vousolo <vousolo.guia@gmail.com>
2026-03-11 00:09:35 -0300

    Add list of EU airports to airports.py

---
+++ airports.py
@@ -0,0 +1,28 @@
+EU_AIRPORTS = [
+
+# Espanha
+"MAD",  # Madrid
+"BCN",  # Barcelona
+"VLC",  # Valencia
+"BIO",  # Bilbao
+
+# Itália norte
+"MXP",  # Milano Malpensa
+"LIN",  # Milano Linate
+"BGY",  # Bergamo
+
+# Itália centro/sul (bons para volta)
+"FCO",  # Roma
+"NAP",  # Napoli
+"PMO",  # Palermo
+"CTA",  # Catania
+"BRI",  # Bari
+
+# outros hubs baratos
+"ZRH",  # Zurich
+"GVA",  # Geneva
+"VIE",  # Vienna
+"PRG",  # Prague
+"ATH",  # Athens
+"BUD"   # Budapest
+]
